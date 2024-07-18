a330f557aec1736d5aef0d3da668b2e544200638
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2024-07-18 20:18:41 +1000

    Extract the relevant value section as a dict

--- remover.py
+++ remover.py
@@ -1,8 +1,18 @@
 import plistlib
 
 # Read the given plist file
-file_name = "Sample.plist"
+file_name = "config.plist"
 with open(file_name, "rb") as fp:
     plist_data = plistlib.load(fp)
 
-print(plist_data)
+# Retrieve original PlatformInfo values
+platforminfo_dict = plist_data["PlatformInfo"]["Generic"]
+print(platforminfo_dict)
+
+# Define key names of sensitive info
+keys_to_remove = [
+    "SystemProductName",
+    "MLB",
+    "SystemSerialNumber",
+    "SystemUUID"
+]
