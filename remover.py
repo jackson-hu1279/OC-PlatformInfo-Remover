bf3c84dad5b5758611e4d8b09e3bdd10b416f002
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2024-07-20 17:12:22 +1000

    Add batch processing mode

--- remover.py
+++ remover.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import argparse
 import plistlib
 
@@ -58,7 +59,46 @@ def save_new_plist_file(original_file_name, plist_data):
         plistlib.dump(plist_data, fp)
 
 
+# List all files in a directory
+def list_files(directory):
+    try:
+        # Keep only plist files
+        file_list = os.listdir(directory)
+        files = [
+            file
+            for file in file_list
+            if os.path.isfile(os.path.join(directory, file)) and file.endswith(".plist")
+        ]
+
+        if DEBUG:
+            print(f"Plist files found in directory: '{files}'")
+
+        return files
+    except FileNotFoundError:
+        print(f"Error: Directory '{directory}' does not exist")
+    except PermissionError:
+        print(f"Error: Permission denied to access the directory '{directory}'")
+    except Exception as e:
+        print(f"An error occurred when listing files in '{directory}'")
+        if DEBUG:
+            print(f"Error message: {e}")
+
+
+# Process a single plist file
+def process_plist_file(file_name):
+    # Load the given plist file
+    plist_data = load_plist_file(file_name)
+
+    # Remove sensitive PlatformInfo values
+    plist_data_updated = remove_platform_values(plist_data)
+
+    # Save the modified plist as a new file
+    save_new_plist_file(original_file_name=file_name, plist_data=plist_data_updated)
+
+
 def main(args):
+    global DEBUG
+
     # Check parsed args
     file_name = args.file if args.file else PLIST_FILE_NAME
     dir_path = args.dir
@@ -69,14 +109,15 @@ def main(args):
         print(f"Directory path: '{args.dir}'")
         print(f"Verbose mode: '{args.verbose}'")
 
-    # Load the given plist file
-    plist_data = load_plist_file(file_name)
+    # Enter batch processing mode if given dir path
+    if dir_path:
+        file_name_lst = list_files(dir_path)
+        for file_name in file_name_lst:
+            process_plist_file(dir_path + "/" + file_name)
 
-    # Remove sensitive PlatformInfo values
-    plist_data_updated = remove_platform_values(plist_data)
-
-    # Save the modified plist as a new file
-    save_new_plist_file(original_file_name=file_name, plist_data=plist_data_updated)
+    # Otherwise only process single plist file
+    else:
+        process_plist_file(file_name)
 
 
 if __name__ == "__main__":
